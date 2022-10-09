b28ab0cf57bccb5734985982d7d8359ae3c6a652
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-10-09 14:12:22 +0800

    Map implmentation and useReducer input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Input from '../../shared/components/FormElements/Input';
+
+import './NewPlace.css';
+
 const NewPlace = () => {
     return(
-        <h2>
-            New place works!
-        </h2>
+        <form className="place-form">
+            <Input element="input" type="text" label="Title" validators={[]} errorText="Please enter a valid title!" />
+        </form>
     );
 };
 
