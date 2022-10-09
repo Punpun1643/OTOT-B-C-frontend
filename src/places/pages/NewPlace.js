304a847c106bd2123acb121b638e3388eb526722
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-10-10 02:07:44 +0800

    Complete add place

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -69,7 +69,7 @@ const NewPlace = () => {
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title."
+        errorText="Please enter a valid title!"
         onInput={inputHandler}
       />
       <Input
@@ -77,7 +77,15 @@ const NewPlace = () => {
         element="textarea"
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description (at least 5 characters)."
+        errorText="Please enter a valid description (at least 5 characters)!"
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address!"
         onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>
